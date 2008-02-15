7323cda2fcbce338a120b838027a126b72cca154
Will Guaraldi <willg@bluesock.org>
2008-02-15 22:46:38 +0000

    Re-worked the compile.sh script. Added a link to mod_wsgi.  Thanks Klaus!

--- compile.sh
+++ compile.sh
@@ -7,9 +7,24 @@
 #
 # where "xyz" is the directory to PyBlosxom.
 
-rm -rf ./compiled_site/
-mkdir ./logs/
+# rm -rf ./compiled_site/
 
-python ./pyblosxom.cgi --static
+if [[ -d ./logs/ ]]
+    then echo "logs directory exists."
+    else
+        echo "logs directory doesn't exist--creating."
+        mkdir ./logs/
+fi
+
+if [[ -d ./compiled_site/ ]]
+    then
+        echo "compiled_site exists, doing incremental static rendering...."
+        python ./pyblosxom.cgi --static incremental
+    else
+        echo "compiled_site doesn't exist, doing full static rendering...."
+        mkdir ./compiled_site/
+        python ./pyblosxom.cgi --static
+fi
 
 cp -r ./htdocs/* ./compiled_site/
+find ./compiled_site/ -name ".svn" -exec 'rm' '-rf' '{}' ';'
