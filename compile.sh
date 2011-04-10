510b729675f4bd843df42181ae63f62750711a5a
Will Kahn-Greene <willg@bluesock.org>
2011-04-10 10:38:17 -0400

    Tweaks indenting so it's easier for me to read.

--- compile.sh
+++ compile.sh
@@ -8,21 +8,22 @@
 # where "xyz" is the directory to PyBlosxom.
 
 if [[ -d ./logs/ ]]
-    then echo "logs directory exists."
-    else
-        echo "logs directory doesn't exist--creating."
-        mkdir ./logs/
+then
+    echo "logs directory exists."
+else
+    echo "logs directory doesn't exist--creating."
+    mkdir ./logs/
 fi
 
 if [[ -d ./compiled_site/ ]]
-    then
-        echo "compiled_site exists, doing incremental static rendering...."
-        # PYTHONPATH=../pyblosxom/ python ./pyblosxom.cgi staticrender --incremental
-        PYTHONPATH=../pyblosxom/ python ./pyblosxom.cgi staticrender
-    else
-        echo "compiled_site doesn't exist, doing full static rendering...."
-        mkdir ./compiled_site/
-        PYTHONPATH=../pyblosxom/ python ./pyblosxom.cgi staticrender
+then
+    echo "compiled_site exists, doing incremental static rendering...."
+    # PYTHONPATH=../pyblosxom/ python ./pyblosxom.cgi staticrender --incremental
+    PYTHONPATH=../pyblosxom/ python ./pyblosxom.cgi staticrender
+else
+    echo "compiled_site doesn't exist, doing full static rendering...."
+    mkdir ./compiled_site/
+    PYTHONPATH=../pyblosxom/ python ./pyblosxom.cgi staticrender
 fi
 
 # copies over all the bits from htdocs to compiled_site if they aren't
